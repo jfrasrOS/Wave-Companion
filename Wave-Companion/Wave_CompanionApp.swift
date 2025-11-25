d949f12755e3afe4ef6e31748dbe042e725e0576
johnfraser <burton_fsr@icloud.com>
2025-11-25 15:31:57 +0100

    Initial Commit

---
+++ Wave-Companion/Wave_CompanionApp.swift
@@ -0,0 +1,32 @@
+//
+//  Wave_CompanionApp.swift
+//  Wave-Companion
+//
+//  Created by John on 25/11/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Wave_CompanionApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
