82a78b02196146c212cdb22ca353ecf63f2307b7
johnfraser <burton_fsr@icloud.com>
2025-12-01 20:52:39 +0100

    Mise en place des models

---
+++ Wave-Companion/Models/Spot.swift
@@ -0,0 +1,8 @@
+//
+//  Spot.swift
+//  Wave-Companion
+//
+//  Created by John on 01/12/2025.
+//
+
+import Foundation
---
+++ Wave-Companion/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  Wave-Companion
+//
+//  Created by John on 01/12/2025.
+//
+
+import Foundation
