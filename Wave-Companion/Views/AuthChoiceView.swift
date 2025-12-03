0bbb7e307f51545ba509a2bd43a434a6ee0213a5
johnfraser <burton_fsr@icloud.com>
2025-12-03 16:04:23 +0100

    LoadingView + AuthChoiceView + début logique entrée app

---
+++ Wave-Companion/Views/AuthChoiceView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthChoiceView.swift
+//  Wave-Companion
+//
+//  Created by John on 03/12/2025.
+//
+
+import SwiftUI
+
+struct AuthChoiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthChoiceView()
+}
